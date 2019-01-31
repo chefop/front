e50225dbba216276a673ffb8993bc1c4285ec960
Louis Cuvelier <lcuvelier33@gmail.com>
2019-01-31 17:26:58 +0100

    feat(room): vue vide pour la salle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Owner from './Owner';
+import Room from "./Room";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Owner />
+        <Room />
       </>
     );
   }
