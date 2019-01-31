fa8fd88a35751d477639ff1053ef23ab31082d6d
Cédric Prezelin <cedric.prezelin@outlook.com>
2019-01-31 11:41:45 +0100

    correctif de linting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import reducer from './store/ducks'
+
+const store = createStore(reducer);
 
 // Components
 import App from './pages';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
