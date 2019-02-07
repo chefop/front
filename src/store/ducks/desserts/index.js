ff26bd5e2b3ec39851d82e9a04811338cbde3f80
Unknown <marc.priolot@ynov.com>
2019-02-07 16:55:06 +0100

    feat(dessert): ajout : add - fetch - update - delete pour dessert, Redux

---
+++ src/store/ducks/desserts/index.js
@@ -0,0 +1,91 @@
+import {
+  phasedActionTypes,
+  phasedActionCreators,
+} from '../../../utils/actionsHelper';
+
+// Action types
+export const ADD_DESSERT = phasedActionTypes('dessert/ADD_DESSERT');
+export const FETCH_DESSERTS = phasedActionTypes('dessert/FETCH_DESSERT');
+export const UPDATE_DESSERT = phasedActionTypes('dessert/UPDATE_DESSERT');
+export const DELETE_DESSERT = phasedActionTypes('dessert/DELETE_DESSERT');
+
+// Action creators
+export const addDessert = phasedActionCreators(ADD_DESSERT);
+export const fetchDesserts = phasedActionCreators(FETCH_DESSERTS);
+export const updateDessert = phasedActionCreators(UPDATE_DESSERT);
+export const deleteDessert = phasedActionCreators(DELETE_DESSERT);
+
+// Initial State
+const initialState = {
+  desserts: [
+    {
+      name: 'Crêpe au sucre',
+      description:
+        'Tu as de la pate ? Tu as du sucre ? Beh tu met la pate avec du suc dessus',
+      dfPrice: 2.8,
+      vat: 0.2,
+      quantity: 46,
+      allergen: ['9080129809', '0980980980'],
+      photo:
+        'https://www.recettes.net/documents/upload/original/c/r/crepe-au-sucre.jpg',
+    },
+    {
+      name: 'Browkies',
+      description:
+        'Un genre de Brownie au Cookies. En vrai, de toi à moi, une tuerie !!',
+      dfPrice: 2.5,
+      vat: 0.2,
+      quantity: 24,
+      allergen: ['098091809', '1313139939'],
+      photo:
+        'https://static.cuisineaz.com/400x320/i140676-brownie-cookie-chocolat-noix-de-pecan.jpeg',
+    },
+  ],
+  error: {},
+};
+
+const dessertReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_DESSERT.success:
+      return {
+        ...state,
+        desserts: [...state.desserts, action.payload],
+      };
+    case FETCH_DESSERTS.success:
+      return {
+        ...state,
+        desserts: action.payload,
+      };
+    case UPDATE_DESSERT:
+      return {
+        ...state,
+        desserts: [
+          ...state.desserts.filter(
+            (desserts) => desserts._id !== action.payload._id,
+          ),
+          action.payload,
+        ],
+      };
+    case DELETE_DESSERT.success:
+      return {
+        ...state,
+        desserts: [
+          ...state.desserts.filter(
+            (desserts) => desserts._id !== action.payload._id,
+          ),
+        ],
+      };
+    case ADD_DESSERT.failure:
+    case FETCH_DESSERTS.failure:
+    case UPDATE_DESSERT.failure:
+    case DELETE_DESSERT.failure:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default dessertReducer;
---
+++ src/store/ducks/desserts/operations.js
@@ -0,0 +1,88 @@
+import { call, fork, put, takeLatest, all } from 'redux-saga/effects';
+
+// Import action type
+import { ADD_DESSERT, FETCH_DESSERTS, UPDATE_DESSERT, DELETE_DESSERT } from '.';
+
+// Import action creators
+import { addDessert, fetchDesserts, updateDessert, deleteDessert } from '.';
+
+import * as dessertAPI from '../../APICalls/dessertsAPI';
+
+// WORKERS
+function* addDessertWorker(action) {
+  try {
+    const dessert = action.payload;
+    const res = yield call(dessertAPI.createDessert, dessert);
+    if (res.status === 200) {
+      const dessert = res.data.starter;
+      yield put(addDessert.success(dessert()));
+    }
+  } catch (err) {
+    yield put(addDessert.failure(err.message));
+  }
+}
+
+function* fetchDessertsWorker() {
+  try {
+    const res = yield call(dessertAPI.fetchDesserts);
+    if (res.status === 200) {
+      const dessert = res.data.starter;
+      yield put(fetchDesserts.success(dessert()));
+    }
+  } catch (err) {
+    yield put(fetchDesserts.failure(err.message));
+  }
+}
+
+function* updateDessertWorker(action) {
+  try {
+    const dessert = action.payload;
+    const res = yield call(dessertAPI.updateDessert, dessert);
+    if (res.status === 200) {
+      const dessert = res.data.starter;
+      yield put(updateDessert.success(dessert()));
+    }
+  } catch (err) {
+    yield put(updateDessert.failure(err.message));
+  }
+}
+
+function* deleteDessertWorker(action) {
+  try {
+    const dessert = action.payload;
+    const res = yield call(dessertAPI.deleteDessert, dessert);
+    if (res.status === 200) {
+      const dessert = res.data.starter;
+      yield put(deleteDessert.success(dessert()));
+    }
+  } catch (err) {
+    yield put(deleteDessert.failure(err.message));
+  }
+}
+
+// WATCHERS
+function* addDessertSaga() {
+  yield takeLatest(ADD_DESSERT.request, addDessertWorker);
+}
+
+function* fetchDessertsSaga() {
+  yield takeLatest(FETCH_DESSERTS.request, fetchDessertsWorker);
+}
+
+function* updateDessertSaga() {
+  yield takeLatest(UPDATE_DESSERT.request, updateDessertWorker);
+}
+
+function* deleteDessertSaga() {
+  yield takeLatest(DELETE_DESSERT.request, deleteDessertWorker);
+}
+
+// Export watchers
+export default function* dessertSagas() {
+  yield all(
+    [fork(addDessertSaga)],
+    [fork(fetchDessertsSaga)],
+    [fork(updateDessertSaga)],
+    [fork(deleteDessertSaga)],
+  );
+}
