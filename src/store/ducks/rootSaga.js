1101fb9425d2683bff44a3280a238796ba248a01
Unknown <marc.priolot@ynov.com>
2019-02-07 15:15:29 +0100

    fix: typo

--- src/store/ducks/rootSaga.js
+++ src/store/ducks/rootSaga.js
@@ -3,7 +3,7 @@ import { all, fork } from 'redux-saga/effects';
 // Import all sagas
 import starterSaga from './starters/operations';
 import mainCourseSaga from './mainCourses/operations';
-import drinkSaga from './drink/operations';
+import drinkSaga from './drinks/operations';
 
 export default function* rootSaga() {
   yield all([fork(starterSaga)], [fork(mainCourseSaga)], [fork(drinkSaga)]);
