6886bc2068adc17e61f0e06748b6f3a55847fd60
Cédric Prezelin <cedric.prezelin@outlook.com>
2019-02-07 19:38:59 +0100

    feat: ajout de tests Redux-Saga des starters

--- src/store/ducks/starters/index.js
+++ src/store/ducks/starters/index.js
@@ -5,14 +5,12 @@ import {
 
 // Action types
 export const ADD_STARTER = phasedActionTypes('starter/ADD_STARTER');
-export const FETCH_STARTER = phasedActionTypes('starter/FETCH_STARTER');
 export const FETCH_STARTERS = phasedActionTypes('starter/FETCH_STARTERS');
 export const UPDATE_STARTER = phasedActionTypes('starter/UPDATE_STARTER');
 export const DELETE_STARTER = phasedActionTypes('starter/DELETE_STARTER');
 
 // Action creators
 export const addStarter = phasedActionCreators(ADD_STARTER);
-export const fetchStarter = phasedActionCreators(FETCH_STARTER);
 export const fetchStarters = phasedActionCreators(FETCH_STARTERS);
 export const updateStarter = phasedActionCreators(UPDATE_STARTER);
 export const deleteStarter = phasedActionCreators(DELETE_STARTER);
@@ -64,7 +62,6 @@ const initialState = {
 const starterReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_STARTER.success:
-    case FETCH_STARTER.success:
       return {
         ...state,
         starters: [...state.starters, action.payload],
@@ -92,7 +89,6 @@ const starterReducer = (state = initialState, action) => {
         ],
       };
     case ADD_STARTER.failure:
-    case FETCH_STARTER.failure:
     case FETCH_STARTERS.failure:
     case UPDATE_STARTER.failure:
     case DELETE_STARTER.failure:
