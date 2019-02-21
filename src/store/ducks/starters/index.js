6af46b85dd106af11cbd204307b488a6ad572d53
Cédric Prezelin <cedric.prezelin@outlook.com>
2019-02-21 17:01:30 +0100

    fix: typo

--- src/store/ducks/starters/index.js
+++ src/store/ducks/starters/index.js
@@ -24,7 +24,7 @@ const initialState = {
       dfPrice: 7.4,
       vat: 0.2,
       quantity: 1000,
-      allergen: ['allergen1', 'allergen2'],
+      allergen: [{ _id: '312223', name: 'noix de pécan' }],
       photo:
         'https://img-3.journaldesfemmes.fr/QhgEdLZ0suRdfP7pAM6vG9ECfvo=/750x/smart/3a86b25b4fd94561959d9ff592bce391/recipe-jdf/10025061.jpg',
       _id: '891728109831',
@@ -48,7 +48,14 @@ const initialState = {
       dfPrice: 10.3,
       vat: 0.2,
       quantity: 2,
-      allergen: ['allergen2'],
+      allergen: [
+        { _id: '1233123', name: 'lactose' },
+        { _id: '123311322223', name: 'gluten' },
+        { _id: '12331223', name: 'noix' },
+        { _id: '123312223', name: 'fruit à coque' },
+        { _id: '1233fgd12232223', name: 'farine' },
+        { _id: '123312223', name: 'fruit de mer' },
+      ],
       photo:
         'https://www.papillesetpupilles.fr/wp-content/uploads/2010/03/Millefeuille-de-betterave-au-fromage-de-chevre-600x792.jpg',
       _id: '75065673709092536',
@@ -59,7 +66,7 @@ const initialState = {
       dfPrice: 7.4,
       vat: 0.2,
       quantity: 1000,
-      allergen: ['allergen1', 'allergen2'],
+      allergen: [{ _id: '312223', name: 'noix de pécan' }],
       photo:
         'https://img-3.journaldesfemmes.fr/QhgEdLZ0suRdfP7pAM6vG9ECfvo=/750x/smart/3a86b25b4fd94561959d9ff592bce391/recipe-jdf/10025061.jpg',
       _id: '891728109831',
@@ -83,7 +90,14 @@ const initialState = {
       dfPrice: 10.3,
       vat: 0.2,
       quantity: 2,
-      allergen: ['allergen2'],
+      allergen: [
+        { _id: '1233123', name: 'lactose' },
+        { _id: '123311322223', name: 'gluten' },
+        { _id: '12331223', name: 'noix' },
+        { _id: '123312223', name: 'fruit à coque' },
+        { _id: '1233fgd12232223', name: 'farine' },
+        { _id: '123312223', name: 'fruit de mer' },
+      ],
       photo:
         'https://www.papillesetpupilles.fr/wp-content/uploads/2010/03/Millefeuille-de-betterave-au-fromage-de-chevre-600x792.jpg',
       _id: '75065673709092536',
@@ -94,7 +108,7 @@ const initialState = {
       dfPrice: 7.4,
       vat: 0.2,
       quantity: 1000,
-      allergen: ['allergen1', 'allergen2'],
+      allergen: [{ _id: '312223', name: 'noix de pécan' }],
       photo:
         'https://img-3.journaldesfemmes.fr/QhgEdLZ0suRdfP7pAM6vG9ECfvo=/750x/smart/3a86b25b4fd94561959d9ff592bce391/recipe-jdf/10025061.jpg',
       _id: '891728109831',
@@ -118,7 +132,56 @@ const initialState = {
       dfPrice: 10.3,
       vat: 0.2,
       quantity: 2,
-      allergen: ['allergen2'],
+      allergen: [
+        { _id: '1233123', name: 'lactose' },
+        { _id: '123311322223', name: 'gluten' },
+        { _id: '12331223', name: 'noix' },
+        { _id: '123312223', name: 'fruit à coque' },
+        { _id: '1233fgd12232223', name: 'farine' },
+        { _id: '123312223', name: 'fruit de mer' },
+      ],
+      photo:
+        'https://www.papillesetpupilles.fr/wp-content/uploads/2010/03/Millefeuille-de-betterave-au-fromage-de-chevre-600x792.jpg',
+      _id: '75065673709092536',
+    },
+    {
+      name: 'Salade niçoise 🥗',
+      description: 'Une salade faite à Nice',
+      dfPrice: 7.4,
+      vat: 0.2,
+      quantity: 1000,
+      allergen: [{ _id: '312223', name: 'noix de pécan' }],
+      photo:
+        'https://img-3.journaldesfemmes.fr/QhgEdLZ0suRdfP7pAM6vG9ECfvo=/750x/smart/3a86b25b4fd94561959d9ff592bce391/recipe-jdf/10025061.jpg',
+      _id: '891728109831',
+    },
+    {
+      name: 'Foie gras 🦆',
+      description:
+        'Il était une fois un foie se prêtant ma foi à une degustation foireuse.',
+      dfPrice: 10.3,
+      vat: 0.2,
+      quantity: 78,
+      allergen: [],
+      photo:
+        'https://static.fermedebeaumont.com/images/fermedebeaumont/mandarin-logo/mandarin4.jpg',
+      _id: '908098327467072',
+    },
+    {
+      name: 'On a tenté un truc 🐐',
+      description:
+        "C'est la surprise (C'est un millefeuille… Avec de la betterave… et du fromage de chèvre…)",
+      dfPrice: 10.3,
+      vat: 0.2,
+      quantity: 2,
+      allergen: [
+        { _id: '1233123', name: 'lactose' },
+        { _id: '123311322223', name: 'gluten' },
+        { _id: '12331223', name: 'noix' },
+        { _id: '123312223', name: 'fruit à coque' },
+        { _id: '1233fgd12232223', name: 'farine' },
+        { _id: '123312223', name: 'fruit de mer' },
+      ],
       photo:
         'https://www.papillesetpupilles.fr/wp-content/uploads/2010/03/Millefeuille-de-betterave-au-fromage-de-chevre-600x792.jpg',
       _id: '75065673709092536',
